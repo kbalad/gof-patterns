88d307a61096878476d2066e7de68b97ff28d5d1
kbalad <kirillbalad@gmail.com>
2023-06-23 14:19:16 +0200

    add chain of responsibility

--- src/main/java/org/example/behavioral/chainofresponsibility/ATMDispenseChain.java
+++ src/main/java/org/example/behavioral/chainofresponsibility/ATMDispenseChain.java
@@ -8,9 +8,11 @@ public class ATMDispenseChain {
         root = new Dollar50Dispenser();
         DispenseChain d2 = new Dollar20Dispenser();
         DispenseChain d3 = new Dollar10Dispenser();
+        DispenseChain d4 = new Dollar1Dispenser();
 
         root.setNextChain(d2);
         d2.setNextChain(d3);
+        d3.setNextChain(d4);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/behavioral/chainofresponsibility/Dollar1Dispenser.java
@@ -0,0 +1,9 @@
+package org.example.behavioral.chainofresponsibility;
+
+public class Dollar1Dispenser extends BaseDispenseChain {
+
+    public Dollar1Dispenser() {
+        super(1);
+    }
+
+}
